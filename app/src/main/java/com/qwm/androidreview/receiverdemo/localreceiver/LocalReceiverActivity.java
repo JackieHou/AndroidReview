51aedae565fda6013988f4430a6a741e38db1b28
qiwenming <qiwenmingshiwo@163.com>
2017-06-15 18:16:42 +0800

    [数据存储]

--- app/src/main/java/com/qwm/androidreview/receiverdemo/localreceiver/LocalReceiverActivity.java
+++ app/src/main/java/com/qwm/androidreview/receiverdemo/localreceiver/LocalReceiverActivity.java
@@ -3,7 +3,6 @@ package com.qwm.androidreview.receiverdemo.localreceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
